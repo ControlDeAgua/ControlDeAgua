655d5a4dc11cd35f484fc33577a03a17e8664377
Diego Ramirez <dr01191115@gmail.com>
2021-08-27 14:04:59 -0500

    Update manage-products.py
    Complete the main menu.

--- manage-products.py
+++ manage-products.py
@@ -19,7 +19,7 @@ class ProductManager:
         self.product_dict = get_product_dict()
         self.main_menu()
     
-    def get_product_info(self) -> None:
+    def reload_product_info(self) -> None:
         """
         Open the file stream about product information. Even
         when this stream does not affect the main form
@@ -40,12 +40,47 @@ class ProductManager:
         - Manage an existent product
         - Delete an existent product
         - Register a new product
+        - View the current products
         - Exit
         """
+        self.menu_frame = Frame(self.root)
+        self.menu_frame.grid()
+        # a greetings label
+        welcome_l = Label(self.menu_frame, text="""Seleccione una opcion para manejar la
+informacion de los productos utilizada
+en las operaciones de venta y reporte.""", font=("Calibri", "14", "bold"), bg="white", fg="black").grid(row=0, column=0, sticky="ew")
+        # option to manage
+        manage = Button(self.menu_frame, text="Manejar un producto que ya existe", fg="black", bg="whitesmoke",
+        font=("Calibri", "14", "bold"), command=lambda: self.move_to_option("home", "manage")).grid(row=1, column=0, sticky="ew")
+        # option to delete
+        delete = Button(self.menu_frame, text="Eliminar un producto de la lista", fg="black", bg="whitesmoke",
+        font=("Calibri", "14", "bold"), command=lambda: self.move_to_option("home", "delete")).grid(row=2, column=0, sticky="ew")
+        # option to create
+        create = Button(self.menu_frame, text="Agregar un producto a la lista", fg="black", bg="whitesmoke",
+        font=("Calibri", "14", "bold"), command=lambda: self.move_to_option("home", "create")).grid(row=3, column=0, sticky="ew")
+        # option to show
+        show = Button(self.menu_frame, text="Ver la lista de productos", fg="black", bg="whitesmoke",
+        font=("Calibri", "14", "bold"), command=lambda: self.move_to_option("home", "show")).grid(row=4, column=0, sticky="ew")
+        # option to exit
+        exit = Button(self.menu_frame, text="Salir", fg="white", bg="red",
+        font=("Calibri", "14", "bold"), command=self.root.quit).grid(row=5, column=0, sticky="ew")
     
     def loop(self) -> None:
         "run self.root.mainloop() from the class."
         self.root.mainloop()
+    
+    def finish_message(self, event: str) -> None:
+        "message shown each time we complete an operation here. Ain't so hard."
+        messagebox.showinfo("Proceso completado sin errores", f"""El proceso se ha completado con exito.
+Recuerde cerrar la aplicacion de registro/configuracion y volverla a abrir
+para poder ver los cambios realizados.
+
+(Proceso: {event})""")
+        self.reload_product_info()
+    
+    def move_to_option(self, origin: str, dest: str) -> None:
+        "Move between frames."
+        result = (origin, dest)
 
 if __name__ == "__main__":
     # main level execution
