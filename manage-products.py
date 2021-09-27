86348f4c5c06c0ca46f9c54894c99b8fb1876d22
Diego Ramirez <dr01191115@gmail.com>
2021-09-27 13:56:34 -0500

    Update manage-products.py
    * Use tools.database.ProductMap, not tools.database.get_product_dir.
    * Work on the "manage" option.
    * Manage some critical errors.
    * Modify the update logic.

--- manage-products.py
+++ manage-products.py
@@ -4,6 +4,7 @@ import json
 from idlelib.textview import view_text
 from tkinter import *
 from tkinter import messagebox
+from tools import panic
 from tools.database import get_product_dict
 from tools.prefabricated import get_menubutton
 
@@ -33,8 +34,11 @@ class ProductManager:
         method manually. This is only used to re-force the
         product info).
         """
-        del(self.product_dict)
-        self.product_dict = get_product_dict()
+        try:
+            del(self.product_dict)
+            self.product_dict = ProductMap()
+        except Exception as e:
+            panic.panic_msg(e)
     
     def main_menu(self) -> None:
         """
@@ -72,12 +76,38 @@ en las operaciones de venta y reporte.""", font=("Calibri", "14", "bold"), bg="w
         "manage a product."
         self.manage_frame = Frame(self.root)
         self.manage_frame.grid()
+        self.manage_selection = IntVar()
+        self.manage_new_value = DoubleVar()
+        self.manage_options = self.product_dir.get_list()
         # add a manage_product function
         def manage_product():
-            self.go("manage", "home")
+            try:
+                target, value = self.product_dict.product_index[self.manage_selection.get()], self.manage_new_value.get()
+                self.product_dict.products[target] = value
+                with open("C:/Program Files/Control de Agua/tools/products.json", "w") as f:
+                    f.write(json.dump(self.product_dict.products))
+            except Exception as e:
+                messagebox.showerror("Error", f"""{type(e).__name__}: {str(e)}
+
+Verifique sus entradas e intente de nuevo.""")
+                self.move_to_option("manage", "RETRY")
+            self.finish_message("manage")
+            self.move_to_option("manage", "home")
         # add abels and menubuttons
-        l = Label(self.manage_frame, text="Elija un producto:", bg="whitesmoke", fg="black",
+        l = Label(self.manage_frame, text="1. Elija un producto:", bg="whitesmoke", fg="black",
         font=("Calibri", "13", "bold")).grid(row=0, column=0, sticky="ew")
+        opts = get_menubutton(self.manage_frame,
+                              self.manage_options,
+                              self.manage_selection,
+                              row=0,
+                              column=1)
+        l2 = Label(self.manage_frame, text="2. Introduzca un valor para reemplazar (pesos mexicanos):", bg="whitesmoke",
+        fg="black", font=("Calibri", "13", "bold")).grid(row=1, column=0, sticky="ew")
+        new_value = Entry(self.manage_frame, textvariable=self.manage_new_value, width=40).grid(row=1, column=1, sticky="ew")
+        cancel = Button(self.manage_frame, text="Cancelar", bg="red", fg="whitesmoke",
+        command=lambda:self.move_to_option("manage", "home")).grid(row=2, column=0, sticky="ew")
+        move_it = Button(self.manage_frame, text="Modificar", bg="cyan",  # i decided to use cyan instead of green!
+        fg="whitesmoke", command=manage_product).grid(row=2, column=1, sticky="ew")
 
     def loop(self) -> None:
         "run self.root.mainloop() from the class."
@@ -90,7 +120,6 @@ Recuerde cerrar la aplicacion de registro/configuracion y volverla a abrir
 para poder ver los cambios realizados.
 
 (Proceso: {event})""")
-        self.reload_product_info()
     
     def move_to_option(self, origin: str, dest: str) -> None:
         "Move between frames."
@@ -99,7 +128,7 @@ para poder ver los cambios realizados.
         if result == ("home", "show"):
             # an easy one: just show the product list
             text = ""
-            for k, v in self.product_dict:
+            for k, v in self.product_dict.products.values():
                 text += f"{'='*60}\n- Producto: {k}\n- Valor monetario: ${v[0]}\n- Valor en la cuenta del odometro: {v[1]}\n"
             view_text("Lista de productos registrados", text)
             del(text)
@@ -110,10 +139,16 @@ para poder ver los cambios realizados.
         elif result == ("manage", "home"):
             self.manage_frame.grid_remove()
             self.main_menu()
+        elif result == ("manage", "RETRY"):
+            # retry the "manage" operations
+            self.reload_product_info()
+            self.manage()
         else:
             # no results? you should say it
             messagebox.showwarning("NotImplemented Error", f"""La opcion del menu no fue identificada: {result}.
 Comuniquese con el desarrollador para manejar esta incompatibilidad.""")
+        # always reload the info, to avoid mistakes
+        self.reload_product_info()
 
 if __name__ == "__main__":
     # main level execution
