3415f1d3d88f6c4f601ca1734a057ce9406ab58b
Diego Ramirez <dr01191115@gmail.com>
2021-11-29 12:28:26 -0600

    Add "pathfinders"
    Search by relative paths, not absolute.

--- tools/pathfinders.py
+++ tools/pathfinders.py
@@ -1,4 +1,4 @@
-# Identify the directory where the executables are stored.
+# Identify the directory where the executables are stored, and some other stuff.
 
 import os
 import sys
@@ -47,11 +47,12 @@ def identify_dir(py_name: str, exe_name: str) -> Optional[str]:
       (3, 7),
       (3, 8),
       (3, 9),
-      (3, 10)  # start to consider Python 3.10
+      (3, 10)
     ]
     win_platforms = ["win32", "win-amd64"]
     found_dir = False
-    current_path = "C:/Program Files/Control de Agua"
+    current_path = os.path.dirname()[-1] if os.path.dirname().endswith("/") or os.path.dirname().endswith("\ ".strip()) else os.path.dirname()
+    current_path = "." if current_path.split(".")[-1] not in win_platforms else "../../"
     found_path = ""
     for version in python_versions:
         formatted_version = get_str_python_version(version)
@@ -64,9 +65,24 @@ def identify_dir(py_name: str, exe_name: str) -> Optional[str]:
 
     if not found_dir:
         if not os.path.exists(f"{current_path}/{py_name}"):
-            messagebox.showerror("Error interno", f"""Error fatal: No se pudo hallar la app: {(py_name, exe_name)}.
+            messagebox.showerror("Error interno", f"""Error fatal: No se pudo hallar la app {(py_name, exe_name)} en {current_path}.
 Por favor reporte esto en <https://github.com/ControlDeAgua/bug_tracker/issues>""")
             return None
         else:
             found_path = f"{current_path}/{py_name}"
     return found_path
+
+
+def find_our_file(path: str) -> str:
+    """
+    Find a program or resource, that should
+    live in the ControlDeAgua folder.
+    """
+    if os.path.exists(path):
+        return f"./{path}"
+    elif os.path.exists(f"../../{path}"):
+        return f"../../{path}"
+    else:
+        messagebox.showerror("Error interno", f"""Error fatal: No se pudo hallar el recurso: '{path}'.
+Por favor reporte esto en <https://github.com/ControlDeAgua/bug_tracker/issues>""")
+        return None
